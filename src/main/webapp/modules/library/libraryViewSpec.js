67daeff1a189c70e055638924a69cfe4937f0a31
Sveinung Dalatun <sdalatun@gmail.com>
2013-08-02 21:04:25 +0200

    Fix tests after Backbonifying the app

--- src/main/webapp/modules/library/libraryViewSpec.js
+++ src/main/webapp/modules/library/libraryViewSpec.js
@@ -1,49 +1,47 @@
 define(function(require) {
 
-    var LibraryView = require('modules/library/libraryView'),
-        LibraryRepository = require('modules/library/libraryRepository'),
-        $ = require('jquery'),
-        sinon = require('sinon'),
-        responseFaker = require('responseFaker');
+    var $ = require('jquery'),
+        responseFaker = require('responseFaker'),
+
+        LibraryView = require('modules/library/libraryView')
+        Library = require('modules/library/library');
 
     describe('LibraryView', function() {
         it('has books', function() {
-            var el = $('<div></div>');
-
-            var libraryView = LibraryView({
-                el: el,
-                libraryId: 1,
-                libraryRepository: LibraryRepository()
-            });
+            var library = new Library();
 
             var response = {"books":[{"title":"Of Mice and Men","uri":"/book/1"},{"title":"Sult","uri":"/book/2"}]};
             responseFaker.fakeResponse(response, {}, function() {
-                libraryView.render();
+                library.fetch();
             });
 
-            expect(el.find(".books li").size()).toBe(2);
+            var libraryView = new LibraryView({
+                library: library
+            });
+
+            libraryView.render();
+
+            expect(libraryView.$(".books li").size()).toBe(2);
         });
 
         it('shows the AddBookView', function() {
-            var el = $('<div></div>');
-
-            var libraryView = LibraryView({
-                el: el,
-                libraryId: 1,
-                libraryRepository: LibraryRepository()
-            });
-
-            expect(el.find(".book-input-form")).not.toBe("form");
+            var library = new Library();
 
             var response = {"books":[{"title":"Of Mice and Men","uri":"/book/1"},{"title":"Sult","uri":"/book/2"}]};
             responseFaker.fakeResponse(response, {}, function() {
-                libraryView.render();
+                library.fetch();
             });
 
-            el.find(".add-book").click();
+            var libraryView = new LibraryView({
+                library: library
+            });
 
-            expect(el.find(".book-input-form")).toBe("form");
-            expect(el.find(".book-input-form")).not.toHaveClass("hide");
+            libraryView.render();
+
+            libraryView.$(".add-book").click();
+
+            expect(libraryView.$(".book-input-form")).toBe("form");
+            expect(libraryView.$(".book-input-form")).not.toHaveClass("hide");
         });
     });
 });
