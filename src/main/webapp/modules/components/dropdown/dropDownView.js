1b07cfbd7f2d3a979343aa5128f80e438ab9e29d
Sveinung Dalatun <sdalatun@gmail.com>
2013-08-06 20:32:09 +0200

    Style the dropdown trigger

--- src/main/webapp/modules/components/dropdown/dropDownView.js
+++ src/main/webapp/modules/components/dropdown/dropDownView.js
@@ -4,15 +4,20 @@ define(function(require) {
 
         template = require('text!./dropDownView.mustache');
 
+    require('css!./dropDownView.css');
+
     var DropDownView = BaseView.extend({
         initialize: function(options) {
             this.defaultOption = options.defaultOption;
         },
 
         render: function() {
+            this.$el.addClass('dropdown');
+
             this.renderTemplate(template, {
                 defaultOption: this.defaultOption
             });
+
             return this;
         }
     });
