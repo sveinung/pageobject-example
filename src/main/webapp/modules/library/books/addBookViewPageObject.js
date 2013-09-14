0f803755706a092d7d050b095a9b71de9d950a08
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2013-09-14 19:29:14 +0200

    Move page object helper into its own file

--- src/main/webapp/modules/library/books/addBookViewPageObject.js
+++ src/main/webapp/modules/library/books/addBookViewPageObject.js
@@ -1,49 +1,9 @@
 define(function(require) {
 
-    var _ = require('underscore');
+    var po = require('modules/components/po');
     var sinon = require('sinon');
 
     var dropDownViewPageObject = require('modules/components/dropdown/dropDownViewPageObject');
-
-    var PageObject = function(options) {
-        this.$el = options.$el;
-        _.extend(this, options.attrs || {});
-    };
-
-    _.extend(PageObject.prototype, {
-        expectToBeVisible: function() {
-            expect(this.$el).not.toBeEmpty();
-            expect(this.$el).not.toHaveClass('hide');
-            return this;
-        },
-        expectToBeHidden: function() {
-            expect(this.$el).toHaveClass('hide');
-            return this;
-        }
-    });
-
-    var po = {
-        create: function(attrs) {
-            return function($el) {
-                return new PageObject({
-                    $el: $el,
-                    attrs: attrs
-                });
-            }
-        },
-        input: function(selector) {
-            return function(val) {
-                this.$el.find(selector).val(val).change();
-                return this;
-            }
-        },
-        button: function(selector) {
-            return function() {
-                this.$el.find(selector).click();
-                return this;
-            }
-        }
-    };
 
     return po.create({
         author: po.input(".author-input"),
